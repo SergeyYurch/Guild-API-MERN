e26cf66dfa19a60695c91efee74fc2f6d4185e82
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-01 22:15:44 +0200

    migration to mongoose

--- src/repositories/blogs.repository.ts
+++ src/repositories/blogs.repository.ts
@@ -1,5 +1,5 @@
 import {BlogEntity} from "../services/entities/blog.entity";
-import {blogsCollection} from "../adapters/dbAdapters";
+import {BlogModel, blogsCollection} from "../adapters/dbAdapters";
 import {ObjectId, WithId} from "mongodb";
 import {BlogEditEntity} from "../services/entities/blog-edit.entity";
 import {BlogsRepositoryInterface} from "./interfaces/blogs.repository.interface";
@@ -8,19 +8,18 @@ export const blogsRepository: BlogsRepositoryInterface = {
 
     createNewBlog: async (inputBlog: BlogEntity): Promise<WithId<BlogEntity> | null> => {
         console.log(`[repository]:start createNewBlog`);
-        const result = await blogsCollection.insertOne(inputBlog);
-        return await blogsCollection.findOne({_id: result.insertedId});
+        return await BlogModel.create(inputBlog);
     },
 
     updateBlogById: async (id: string, inputBlog: BlogEditEntity): Promise<boolean> => {
         console.log(`[repository]:start updateBlogById`);
-        const result = await blogsCollection.updateOne({_id: new ObjectId(id)}, {$set: inputBlog});
+        const result = await BlogModel.updateOne({_id: new ObjectId(id)}, {$set: inputBlog});
         return result.acknowledged;
     },
 
     deleteBlogById: async (id: string): Promise<boolean> => {
         console.log(`[repository]:start deleteBlogById`);
-        const result = await blogsCollection.deleteOne({_id: new ObjectId(id)});
+        const result = await BlogModel.deleteOne({_id: new ObjectId(id)});
         return result.acknowledged;
     },
 };
