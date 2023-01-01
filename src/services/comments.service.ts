6bf33eb4ab26f32cc0b2f45ffe3a7de32af77bb3
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-01 22:15:55 +0200

    migration to mongoose

--- src/services/comments.service.ts
+++ src/services/comments.service.ts
@@ -1,17 +1,15 @@
-
-import {usersRepository} from "../repositories/users.repository";
 import {CommentViewModelDto} from "../controllers/dto/commentViewModel.dto";
 import {CommentInputModelDto} from "../controllers/dto/commentInputModel.dto";
 import {CommentEntity} from "./entities/comment.entity";
 import {commentsRepository} from "../repositories/comments.repository";
+import {queryRepository} from '../repositories/query.repository';
 
-const {getUserById} = usersRepository;
 const {createNewUserComment, deleteUserCommentById, editComment} = commentsRepository;
 
 export const commentsService = {
     async createUserComment({content}: CommentInputModelDto, userId: string, postId:string): Promise<CommentViewModelDto | null> {
         const createdAt = new Date().toISOString();
-        const user = await getUserById(userId);
+        const user = await queryRepository.getUserById(userId);
         const userLogin = user!.accountData.login;
         const newUserComment: CommentEntity = {
             userId, createdAt, content, userLogin, postId
