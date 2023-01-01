ff744dc78155d60b529f2d31009e2c4c7380e154
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-01 22:16:58 +0200

    migration to mongoose

--- src/repositories/posts.repository.ts
+++ src/repositories/posts.repository.ts
@@ -1,5 +1,5 @@
 import {PostEntity} from "../services/entities/post.entity";
-import {postsCollection} from "../adapters/dbAdapters";
+import {PostModel, postsCollection} from "../adapters/dbAdapters";
 import {ObjectId, WithId} from "mongodb";
 import {PostEditEntity} from "../services/entities/postEdit.entity";
 import {PostsRepositoryInterface} from "./interfaces/posts.repository.interface";
@@ -8,19 +8,18 @@ export const postsRepository: PostsRepositoryInterface = {
 
     createNewPost: async (inputPost: PostEntity): Promise<WithId<PostEntity> | null> => {
         console.log(`[repository]:start createNewPost`);
-        const result = await postsCollection.insertOne(inputPost);
-        return await postsCollection.findOne({_id: result.insertedId});
+        return await PostModel.create(inputPost);
     },
 
     updatePostById: async (id: string, inputPost: PostEditEntity): Promise<boolean> => {
         console.log(`[repository]:start updatePostById`);
-        const result = await postsCollection.updateOne({_id: new ObjectId(id)}, {$set: inputPost});
+        const result = await PostModel.updateOne({_id: new ObjectId(id)}, {$set: inputPost});
         return result.acknowledged;
     },
 
     deletePostById: async (id: string): Promise<boolean> => {
         console.log(`[repository]:start deletePostById`);
-        const result = await postsCollection.deleteOne({_id: new ObjectId(id)});
+        const result = await PostModel.deleteOne({_id: new ObjectId(id)});
         return result.acknowledged;
     }
 };
