8265c1de7fcc17da9786551b4005f21a7fb1efbd
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-01 22:18:02 +0200

    migration to mongoose

--- src/repositories/interfaces/users.repository.interface.ts
+++ src/repositories/interfaces/users.repository.interface.ts
@@ -1,12 +1,12 @@
 import {UserEntity} from "../../services/entities/user.entity";
-import {UserInDbEntity} from "../entitiesRepository/user-in-db.entity";
+import {UserEntityWithIdInterface} from "../repository-interfaces/user-entity-with-id.interface";
 
 export interface UsersRepositoryInterface {
-    findUserByEmailOrLogin: (loginOrEmail: string) => Promise<UserInDbEntity | null>;
+    findUserByEmailOrLogin: (loginOrEmail: string) => Promise<UserEntityWithIdInterface | null>;
     createNewUser: (user: UserEntity) => Promise<string | null>;
     deleteUserById: (id: string) => Promise<boolean>;
-    getUserById: (id: string) => Promise<UserInDbEntity | null>;
-    findUserByConfirmationCode: (value: string) => Promise<UserInDbEntity | null>;
+    getUserById: (id: string) => Promise<UserEntityWithIdInterface | null>;
+    findUserByConfirmationCode: (value: string) => Promise<UserEntityWithIdInterface | null>;
     confirmEmailInDb: (id: string) => Promise<boolean>;
     updateSendingConfirmEmail: (id: string, confirmationCode: string, expirationDate: Date) => Promise<boolean>;
     setNewConfirmationCode: (id: string, code: string, date: Date) => Promise<boolean>;
