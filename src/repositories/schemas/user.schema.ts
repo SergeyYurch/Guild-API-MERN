2e1c16f197dd6af9ed9afa4fc5ef196ad5714a33
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-01 22:17:51 +0200

    migration to mongoose

---
+++ src/repositories/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+import {Schema} from 'mongoose';
+import {UserEntity} from '../../services/entities/user.entity';
+
+export const userSchema = new Schema<UserEntity>({
+    accountData: {
+        login: String,
+        email: String,
+        passwordHash: String,
+        passwordSalt: String,
+        createdAt: { type: Date, default: Date.now }
+    },
+    emailConfirmation: {
+        confirmationCode: String,
+        expirationDate: Date,
+        isConfirmed: Boolean,
+        dateSendingConfirmEmail: [Date]
+    }
+});
