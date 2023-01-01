2981932e2520ce11962f81139226d4a0562922f6
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-01 22:15:51 +0200

    migration to mongoose

--- src/repositories/comments.repository.ts
+++ src/repositories/comments.repository.ts
@@ -1,23 +1,21 @@
-import {commentsCollection} from "../adapters/dbAdapters";
+import {CommentModel, commentsCollection} from "../adapters/dbAdapters";
 import {ObjectId} from "mongodb";
 import {CommentEntity} from "../services/entities/comment.entity";
 import {CommentInputModelDto} from "../controllers/dto/commentInputModel.dto";
 
 export const commentsRepository = {
     createNewUserComment: async (comment: CommentEntity): Promise<string | null> => {
-        const result = await commentsCollection.insertOne(comment);
-        if (result.acknowledged) return result.insertedId.toString();
-        return null;
+        const result = await CommentModel.create(comment);
+        return result._id.toString() || null;
     },
     deleteUserCommentById: async (id: string): Promise<boolean> => {
         console.log(`[commentsRepository]: delete comment id: ${id}`);
-        const result = await commentsCollection.deleteOne({_id: new ObjectId(id)});
+        const result = await CommentModel.deleteOne({_id: new ObjectId(id)});
         return result.acknowledged;
     },
     editComment:async (id: string, {content}:CommentInputModelDto): Promise<boolean> => {
         console.log(`[commentsRepository]: comment id: ${id} edit`);
-        const result = await commentsCollection.updateOne({_id: new ObjectId(id)},{$set:{content}});
-        console.log(result);
+        const result = await CommentModel.updateOne({_id: new ObjectId(id)},{$set:{content}});
         return result.acknowledged;
     },
 };
