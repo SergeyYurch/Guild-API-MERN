3e9f9d4c316f2429133d09b8ddfb38d9e7a29f1c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-01 22:14:21 +0200

    added schema

---
+++ src/repositories/schemas/comment.schema.ts
@@ -0,0 +1,10 @@
+import {Schema} from 'mongoose';
+import {CommentEntity} from '../../services/entities/comment.entity';
+
+export const commentSchema = new Schema<CommentEntity>( {
+    content: String,
+    postId:String,
+    userId: String,
+    userLogin: String,
+    createdAt: String
+})
