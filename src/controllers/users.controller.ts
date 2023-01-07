340f43428cf021442d999aa3b5d59ed9bcdd501d
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-07 17:53:43 +0200

    fixed

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -27,8 +27,8 @@ usersRouter.get('/:id',
         const id = req.params.id;
         if (!ObjectId.isValid(id)) return res.sendStatus(404);
         const user = await getUserById(id);
-        if (!user) return res.sendStatus(404)
-        return res.status(204).send(user)
+        if (!user) return res.sendStatus(404);
+        return res.status(204).send(user);
     });
 
 
@@ -48,20 +48,22 @@ usersRouter.post('/',
 usersRouter.get('/',
     authBasicMiddleware,
     async (req: Request, res: Response) => {
-    const searchLoginTerm: string | null = req.query.searchLoginTerm ? String(req.query.searchLoginTerm) : null;
-    const searchEmailTerm: string | null = req.query.searchEmailTerm ? String(req.query.searchEmailTerm) : null;
-    const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
-    const result = await getAllUsers(paginatorOption, searchLoginTerm, searchEmailTerm);
-    return res.status(200).json(result);
-});
+        console.log(`[userController]: GET https//{host}/users  init...`);
+        const searchLoginTerm: string | null = req.query.searchLoginTerm ? String(req.query.searchLoginTerm) : null;
+        const searchEmailTerm: string | null = req.query.searchEmailTerm ? String(req.query.searchEmailTerm) : null;
+        const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
+        console.log(searchLoginTerm);
+        const result = await getAllUsers(paginatorOption, searchLoginTerm, searchEmailTerm);
+        return res.status(200).json(result);
+    });
 
 usersRouter.delete('/:id',
     authBasicMiddleware,
     async (req: RequestWithId, res: Response) => {
-    const id = req.params.id;
-    if (!ObjectId.isValid(id)) return res.sendStatus(404);
-    const isExistUser = await getUserById(id);
-    if (!isExistUser) return res.sendStatus(404)
-    const result = await deleteUserById(id);
-    return result ? res.sendStatus(204) : res.sendStatus(401);
-});
+        const id = req.params.id;
+        if (!ObjectId.isValid(id)) return res.sendStatus(404);
+        const isExistUser = await getUserById(id);
+        if (!isExistUser) return res.sendStatus(404);
+        const result = await deleteUserById(id);
+        return result ? res.sendStatus(204) : res.sendStatus(401);
+    });
