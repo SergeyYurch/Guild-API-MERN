2e2d8f4320202105234cabb90e7929d7a0b8d2bc
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-08 13:43:50 +0200

    refactor to classes

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,5 +1,5 @@
 import {UserEntity} from "./entities/user.entity";
-import {usersRepository} from "../repositories/users.repository";
+import {UsersRepository} from "../repositories/users.repository";
 import {UserViewModelDto} from "../controllers/dto/userViewModel.dto";
 import {
     generatePassHash, generateHashSalt,
@@ -8,37 +8,49 @@ import {
     parseUserViewModel
 } from "../helpers/helpers";
 import {emailManager} from "../managers/emailManager";
-import {queryRepository} from '../repositories/query.repository';
 import {UserEntityWithIdInterface} from '../repositories/repository-interfaces/user-entity-with-id.interface';
+import {QueryRepository} from '../repositories/query.repository';
 
-export const usersService = {
-    parseUserViewModel (user: UserEntityWithIdInterface): UserViewModelDto {
+export class UsersService {
+    private usersRepository: UsersRepository;
+    private queryRepository: QueryRepository;
+
+    constructor() {
+        this.usersRepository = new UsersRepository();
+        this.queryRepository = new QueryRepository();
+    }
+
+    parseUserViewModel(user: UserEntityWithIdInterface): UserViewModelDto {
         return {
             id: user.id,
             login: user.accountData.login,
             email: user.accountData.email,
             createdAt: user.accountData.createdAt.toISOString()
         };
-    },
+    }
+
     async deleteUserById(id: string): Promise<boolean> {
         console.log(`[usersService]:deleteUserById started ...`);
-        return await usersRepository.deleteUserById(id);
-    },
+        return await this.usersRepository.deleteUserById(id);
+    }
+
     async findUserByEmailOrLogin(loginOrEmail: string): Promise<UserViewModelDto | null> {
-        const result = await usersRepository.findUserByEmailOrLogin(loginOrEmail);
+        const result = await this.usersRepository.findUserByEmailOrLogin(loginOrEmail);
         if (!result) return null;
-        return usersService.parseUserViewModel(result);
-    },
+        return this.parseUserViewModel(result);
+    }
+
     async getUserById(id: string): Promise<UserViewModelDto | null> {
-        const result = await queryRepository.getUserById(id);
+        const result = await this.queryRepository.getUserById(id);
         if (!result) return null;
-        return usersService.parseUserViewModel(result);
-    },
+        return this.parseUserViewModel(result);
+    }
+
     async createNewUser(login: string, email: string, password: string, confirmed?: boolean): Promise<UserViewModelDto | null> {
         console.log(`[usersService]: createNewUser ${login}`);
         const createdAt = new Date();
         const passwordSalt = await generateHashSalt();
-        const passwordHash = generatePassHash(password, passwordSalt)
+        const passwordHash = generatePassHash(password, passwordSalt);
         const newUser: UserEntity = {
             accountData: {
                 login,
@@ -53,12 +65,13 @@ export const usersService = {
                 isConfirmed: !!confirmed,
                 dateSendingConfirmEmail: [new Date()]
             },
-            passwordRecoveryInformation:null
+            passwordRecoveryInformation: null
         };
-        const user = await usersRepository.createNewUser(newUser);
+        const user = await this.usersRepository.createNewUser(newUser);
         if (!user) return null;
         if (confirmed) return parseUserViewModel(user);
         await emailManager.sendEmailConfirmation(user.accountData.email, user.emailConfirmation.confirmationCode);
-        return usersService.parseUserViewModel(user);
+        return this.parseUserViewModel(user);
     }
-};
+}
+
