58367216a7531c3bfe83c85e1ca0ff5176fecc1b
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-08 13:42:22 +0200

    refactor to classes

--- src/controllers/blogs.controller.ts
+++ src/controllers/blogs.controller.ts
@@ -1,15 +1,15 @@
 import {Router, Request, Response} from "express";
 import {validatorMiddleware} from "../middlewares/validator.middleware";
-import {blogsService} from "../services/blogs.service";
+import {BlogsService} from "../services/blogs.service";
 import {
     RequestWithBody,
     RequestWithId, RequestWithIdAndBody,
 } from "../types/request.type";
 import {BlogInputModelDto} from "./dto/blogInputModel.dto";
-import {queryRepository} from "../repositories/query.repository";
+import {QueryRepository} from "../repositories/query.repository";
 import {PaginatorOptionInterface} from "../repositories/interfaces/query.repository.interface";
 import {parseQueryPaginator} from "../helpers/helpers";
-import {postsService} from "../services/posts.service";
+import {PostsService} from "../services/posts.service";
 import {ObjectId} from "mongodb";
 import {authBasicMiddleware} from "../middlewares/authBasic.middleware";
 
@@ -20,75 +20,70 @@ const {
     validatePostInputModel,
     validateResult
 } = validatorMiddleware;
-const {createNewBlog, editBlogById, deleteBlogById} = blogsService;
-const {createNewPost} = postsService;
-const {getAllBlogs, getBlogById, getPostsForBlog} = queryRepository;
 
+export class BlogsController {
+    private blogsService: BlogsService;
+    private queryRepository: QueryRepository;
+    private postsService:PostsService;
 
-blogsRouter.get('/',
-    async (req: Request, res: Response) => {
+    constructor() {
+        this.blogsService = new BlogsService();
+        this.queryRepository = new QueryRepository();
+        this.postsService = new PostsService()
+    }
+
+    async getBlogs(req: Request, res: Response) {
         console.log(`[blogsController]: ${(new Date()).toISOString()} - start GET:/blogs`);
         try {
             const searchNameTerm = req.query.searchNameTerm ? String(req.query.searchNameTerm) : null;
             const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
-            return res.status(200).json(await getAllBlogs(searchNameTerm, paginatorOption));
+            return res.status(200).json(await this.queryRepository.getAllBlogs(searchNameTerm, paginatorOption));
         } catch (error) {
             return res.sendStatus(500);
         }
-    });
+    }
 
-blogsRouter.post('/',
-    authBasicMiddleware,
-    validateBlogInputModel(),
-    validateResult,
-    async (req: RequestWithBody<BlogInputModelDto>, res: Response) => {
-        console.log(`[blogsController]:route(POST) /blogs/ - run...`);
-        try {
-            const {name, websiteUrl, description} = req.body;
-            const result = await createNewBlog({name, websiteUrl, description});
-            return result ? res.status(201).json(result) : res.sendStatus(500);
-        } catch (error) {
-            return res.sendStatus(500);
-        }
-    });
-
-blogsRouter.get('/:id',
-    async (req: RequestWithId, res: Response) => {
+    async getBlog(req: RequestWithId, res: Response) {
         console.log(`[blogsController]:route(GET) /blogs/:id - run...`);
         try {
             const id = req.params.id;
             console.log(`[blogsController]: ${(new Date()).toISOString()} - start GET:/${id}`);
             if (!ObjectId.isValid(id)) return res.sendStatus(404);
-            const result = await getBlogById(id);
+            const result = await this.queryRepository.getBlogById(id);
             return result ? res.status(200).json(result) : res.sendStatus(404);
         } catch (error) {
             return res.sendStatus(500);
         }
-    });
+    }
 
+    async createBlog(req: RequestWithBody<BlogInputModelDto>, res: Response) {
+        console.log(`[blogsController]:route(POST) /blogs/ - run...`);
+        try {
+            const {name, websiteUrl, description} = req.body;
+            const result = await this.blogsService.createNewBlog({name, websiteUrl, description});
+            return result ? res.status(201).json(result) : res.sendStatus(500);
+        } catch (error) {
+            return res.sendStatus(500);
+        }
+    }
 
-blogsRouter.get('/:id/posts',
-    async (req: RequestWithId, res: Response) => {
+    async getPostsForBlog(req: RequestWithId, res: Response) {
         console.log(`[blogsController]:route(GET) /blogs/:id/posts - run...`);
         try {
             const id = req.params.id;
             console.log(`[blogsController]: ${(new Date()).toISOString()} - start GET:/${id}/posts`);
             if (!ObjectId.isValid(id)) return res.sendStatus(404);
-            const blogIsExist = await getBlogById(id);
+            const blogIsExist = await this.queryRepository.getBlogById(id);
             if (!blogIsExist) return res.sendStatus(404);
             const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
-            const result = await getPostsForBlog(id, paginatorOption);
+            const result = await this.queryRepository.getPostsForBlog(id, paginatorOption);
             return res.status(200).json(result);
         } catch (error) {
             return res.sendStatus(500);
         }
-    });
+    }
 
-blogsRouter.post('/:id/posts',
-    authBasicMiddleware,
-    validatePostInputModel(),
-    validateResult,
-    async (req: RequestWithId, res: Response) => {
+    async createPostForBlog(req: RequestWithId, res: Response) {
         console.log(`[blogsController]:route(POST) /blogs/:id/posts - run...`);
         try {
             const {title, shortDescription, content} = req.body;
@@ -96,45 +91,83 @@ blogsRouter.post('/:id/posts',
             if (!ObjectId.isValid(id)) return res.sendStatus(404);
 
             console.log(`[blogsController]: ${(new Date()).toISOString()} - start POST:/${id}/posts`);
-            const blogIsExist = await getBlogById(id);
+            const blogIsExist = await this.queryRepository.getBlogById(id);
             if (!blogIsExist) return res.sendStatus(404);
-            const result = await createNewPost({title, blogId: id, content, shortDescription});
+            const result = await this.postsService.createNewPost({title, blogId: id, content, shortDescription});
             return res.status(201).json(result);
         } catch (error) {
             return res.sendStatus(500);
         }
-    });
+    }
+
+    async editBlog(req: RequestWithIdAndBody<BlogInputModelDto>, res: Response) {
+        console.log(`[blogsController]:route(PUT) /blogs/:id - run...`);
+        try {
+            const id = req.params.id;
+            if (!ObjectId.isValid(id)) return res.sendStatus(404);
+            const blog = await this.queryRepository.getBlogById(id);
+            if (!blog) return res.sendStatus(404);
+            const {name, websiteUrl, description} = req.body;
+            const inputBlog: BlogInputModelDto = {name, websiteUrl, description};
+            const result = await this.blogsService.editBlogById(id, inputBlog);
+            return !result ? res.sendStatus(500) : res.sendStatus(204);
+        } catch (error) {
+            return res.sendStatus(500);
+        }
+    }
+
+    async deleteBlog(req: RequestWithId, res: Response) {
+        console.log(`[blogsController]:route(DELETE) /blogs/:id - run...`);
+        try {
+            const id = req.params.id;
+            if (!ObjectId.isValid(id)) return res.sendStatus(404);
+            const result = await this.blogsService.deleteBlogById(id);
+            return result ? res.sendStatus(204) : res.sendStatus(404);
+        } catch (error) {
+            return res.sendStatus(500);
+        }
+
+    }
+}
+
+const blogsController = new BlogsController();
+
+
+blogsRouter.get('/',
+    blogsController.getBlogs.bind(blogsController)
+);
+
+blogsRouter.post('/',
+    authBasicMiddleware,
+    validateBlogInputModel(),
+    validateResult,
+    blogsController.createBlog.bind(blogsController)
+);
+
+blogsRouter.get('/:id',
+    blogsController.getBlog.bind(blogsController)
+);
+
+
+blogsRouter.get('/:id/posts',
+    blogsController.getPostsForBlog.bind(blogsController)
+);
+
+blogsRouter.post('/:id/posts',
+    authBasicMiddleware,
+    validatePostInputModel(),
+    validateResult,
+    blogsController.createPostForBlog.bind(blogsController)
+);
 
 blogsRouter.put('/:id',
     authBasicMiddleware,
     validateBlogInputModel(),
     validateResult,
-    async (req: RequestWithIdAndBody<BlogInputModelDto>, res: Response) => {
-        console.log(`[blogsController]:route(PUT) /blogs/:id - run...`);
-        try {
-            const id = req.params.id;
-            if (!ObjectId.isValid(id)) return res.sendStatus(404);
-            const blog = await getBlogById(id);
-            if (!blog) return res.sendStatus(404);
-            const {name, websiteUrl, description} = req.body;
-            const inputBlog: BlogInputModelDto = {name, websiteUrl, description};
-            const result = await editBlogById(id, inputBlog);
-            return !result ? res.sendStatus(500) : res.sendStatus(204);
-        } catch (error) {
-            return res.sendStatus(500);
-        }
-    });
+    blogsController.editBlog.bind(blogsController)
+);
 
 blogsRouter.delete('/:id',
     authBasicMiddleware,
-    async (req: RequestWithId, res: Response) => {
-        console.log(`[blogsController]:route(DELETE) /blogs/:id - run...`);
-        try {
-            const id = req.params.id;
-            if (!ObjectId.isValid(id)) return res.sendStatus(404);
-            const result = await deleteBlogById(id);
-            return result ? res.sendStatus(204) : res.sendStatus(404);
-        } catch (error) {
-            return res.sendStatus(500);
-        }
-    });
+    blogsController.deleteBlog.bind(blogsController)
+);
