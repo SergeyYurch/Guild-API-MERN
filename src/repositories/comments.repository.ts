990cdc71fc2918880a936d7849985a15cc90aee1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-08 13:42:45 +0200

    refactor to classes

--- src/repositories/comments.repository.ts
+++ src/repositories/comments.repository.ts
@@ -3,19 +3,19 @@ import {ObjectId} from "mongodb";
 import {CommentEntity} from "../services/entities/comment.entity";
 import {CommentInputModelDto} from "../controllers/dto/commentInputModel.dto";
 
-export const commentsRepository = {
-    createNewUserComment: async (comment: CommentEntity): Promise<string | null> => {
+export class CommentsRepository {
+    async createNewUserComment(comment: CommentEntity): Promise<string | null> {
         const result = await CommentModel.create(comment);
         return result._id.toString() || null;
-    },
-    deleteUserCommentById: async (id: string): Promise<boolean> => {
+    }
+    async deleteUserCommentById (id: string): Promise<boolean>{
         console.log(`[commentsRepository]: delete comment id: ${id}`);
         const result = await CommentModel.deleteOne({_id: new ObjectId(id)});
         return result.acknowledged;
-    },
-    editComment:async (id: string, {content}:CommentInputModelDto): Promise<boolean> => {
+    }
+    async editComment (id: string, {content}:CommentInputModelDto): Promise<boolean> {
         console.log(`[commentsRepository]: comment id: ${id} edit`);
         const result = await CommentModel.updateOne({_id: new ObjectId(id)},{$set:{content}});
         return result.acknowledged;
-    },
-};
+    }
+}
