eb924ddab4a4b91e05ffdac758366218e2ae13e4
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-08 13:42:56 +0200

    refactor to classes

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,20 +1,20 @@
 import {Router, Request, Response} from "express";
 import {validatorMiddleware} from "../middlewares/validator.middleware";
-import {postsService} from "../services/posts.service";
+import {PostsService} from "../services/posts.service";
 import {PostInputModelDto} from "./dto/postInputModel.dto";
 import {
     RequestWithBody, RequestWithId,
     RequestWithIdAndBody
 } from "../types/request.type";
-import {queryRepository} from "../repositories/query.repository";
+import {QueryRepository} from "../repositories/query.repository";
 import {PaginatorOptionInterface} from "../repositories/interfaces/query.repository.interface";
 import {parseQueryPaginator} from "../helpers/helpers";
 import {authBasicMiddleware} from "../middlewares/authBasic.middleware";
 import {authBearerMiddleware} from "../middlewares/authBearer.middleware";
 import {CommentInputModelDto} from "./dto/commentInputModel.dto";
-import {commentsService} from "../services/comments.service";
+import {CommentsService} from "../services/comments.service";
 import {ObjectId} from "mongodb";
-import {usersService} from "../services/users.service";
+import {UsersService} from "../services/users.service";
 
 export const postsRouter = Router();
 const {
@@ -23,47 +23,43 @@ const {
     validateBlogId,
     validateCommentInputModel
 } = validatorMiddleware;
-const {deletePostById, editPostById, createNewPost} = postsService;
-const {getPostById, getAllPosts, findAllCommentsByPostId} = queryRepository;
-const {getUserById} = usersService;
-const {createUserComment} = commentsService;
 
-postsRouter.get('/', async (req: Request, res: Response) => {
-    const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
-    const posts = await getAllPosts(paginatorOption);
-    return res.status(200).json(posts);
-});
 
-postsRouter.post(
-    '/',
-    authBasicMiddleware,
-    validatePostInputModel(),
-    validateBlogId(),
-    validateResult,
-    async (req: RequestWithBody<PostInputModelDto>, res: Response
-    ) => {
-        const {title, shortDescription, content, blogId} = req.body;
-        const createdPost = await createNewPost({title, shortDescription, content, blogId});
-        return createdPost ? res.status(201).json(createdPost) : res.sendStatus(500);
-    });
+export class PostsController {
+    private queryRepository: QueryRepository;
+    private usersService: UsersService;
+    private commentsService: CommentsService;
+    private postsService:PostsService
 
-postsRouter.get('/:id', async (req: RequestWithId, res: Response) => {
-    const id = req.params.id;
-    if (!ObjectId.isValid(id)) return res.sendStatus(404);
-    const result = await getPostById(id);
-    return result ? res.status(200).json(result) : res.sendStatus(404);
-});
+    constructor() {
+        this.queryRepository = new QueryRepository();
+        this.usersService = new UsersService();
+        this.commentsService = new CommentsService();
+        this.postsService = new PostsService()
+    }
 
-postsRouter.put(
-    '/:id',
-    authBasicMiddleware,
-    validatePostInputModel(),
-    validateBlogId(),
-    validateResult,
-    async (req: RequestWithIdAndBody<PostInputModelDto>, res: Response
-    ) => {
+    async getPosts(req: Request, res: Response) {
+        const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
+        const posts = await this.queryRepository.getAllPosts(paginatorOption);
+        return res.status(200).json(posts);
+    }
+
+    async getPost(req: RequestWithId, res: Response) {
         const id = req.params.id;
-        if (!ObjectId.isValid(id) || !(await getPostById(id))) return res.sendStatus(404);
+        if (!ObjectId.isValid(id)) return res.sendStatus(404);
+        const result = await this.queryRepository.getPostById(id);
+        return result ? res.status(200).json(result) : res.sendStatus(404);
+    }
+
+    async createPost(req: RequestWithBody<PostInputModelDto>, res: Response) {
+        const {title, shortDescription, content, blogId} = req.body;
+        const createdPost = await this.postsService.createNewPost({title, shortDescription, content, blogId});
+        return createdPost ? res.status(201).json(createdPost) : res.sendStatus(500);
+    }
+
+    async editPost(req: RequestWithIdAndBody<PostInputModelDto>, res: Response) {
+        const id = req.params.id;
+        if (!ObjectId.isValid(id) || !(await this.queryRepository.getPostById(id))) return res.sendStatus(404);
         const body = req.body;
         const post: PostInputModelDto = {
             title: body.title,
@@ -71,46 +67,80 @@ postsRouter.put(
             content: body.content,
             shortDescription: body.shortDescription
         };
-        const result = await editPostById(id, post);
+        const result =  await this.postsService.editPostById(id, post);
         return result ? res.sendStatus(204) : res.sendStatus(404);
-    });
+    }
+
+    async deletePost(req: RequestWithId, res: Response) {
+        const id = req.params.id;
+        if (!ObjectId.isValid(id) || !(await this.queryRepository.getPostById(id))) return res.sendStatus(404);
+        const result = await this.postsService.deletePostById(id);
+        return result ? res.sendStatus(204) : res.sendStatus(500);
+    }
+
+    async createCommentForPost(req: RequestWithIdAndBody<CommentInputModelDto>, res: Response) {
+        const postId = req.params.postId;
+        const userId = req.user!.id;
+        if (!userId) return res.sendStatus(401);
+        const userInDb = this.usersService.getUserById(userId);
+        if (!userInDb) return res.sendStatus(401);
+        console.log(`[postsController]: created new comment for post id:${postId} from user id:${userId}`);
+        if (!ObjectId.isValid(postId) || !(await this.queryRepository.getPostById(postId))) {
+            console.log(`[postsController]: wrong post id:${postId}`);
+            return res.sendStatus(404);
+        }
+        const {content} = req.body;
+        const createdComment = await this.commentsService.createUserComment(content, userId, postId);
+        return createdComment ? res.status(201).send(createdComment) : res.sendStatus(500);
+    }
+
+    async getCommentsFOrPost(req: RequestWithIdAndBody<CommentInputModelDto>, res: Response) {
+        const postId = req.params.postId;
+        if (!ObjectId.isValid(postId) || !(await this.queryRepository.getPostById(postId))) return res.sendStatus(404);
+        const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
+        const result = await this.queryRepository.findAllCommentsByPostId(postId, paginatorOption);
+        return result ? res.status(200).json(result) : res.sendStatus(500);
+    }
+}
+
+const postsController = new PostsController;
+
+postsRouter.get('/', postsController.getPosts.bind(postsController));
+
+postsRouter.post(
+    '/',
+    authBasicMiddleware,
+    validatePostInputModel(),
+    validateBlogId(),
+    validateResult,
+    postsController.createPost.bind(postsController)
+);
+
+postsRouter.get('/:id',
+    postsController.getPost.bind(postsController));
+
+postsRouter.put(
+    '/:id',
+    authBasicMiddleware,
+    validatePostInputModel(),
+    validateBlogId(),
+    validateResult,
+    postsController.editPost.bind(postsController)
+);
 
 postsRouter.delete('/:id',
     authBasicMiddleware,
-    async (req: RequestWithId, res: Response) => {
-        const id = req.params.id;
-        if (!ObjectId.isValid(id) || !(await getPostById(id))) return res.sendStatus(404);
-        const result = await deletePostById(id);
-        return result ? res.sendStatus(204) : res.sendStatus(500);
-    });
+    postsController.deletePost.bind(postsController)
+);
 
 postsRouter.post(
     '/:postId/comments',
     authBearerMiddleware,
     validateCommentInputModel(),
     validateResult,
-    async (req: RequestWithIdAndBody<CommentInputModelDto>, res: Response
-    ) => {
-        const postId = req.params.postId;
-        const userId = req.user!.id;
-        if (!userId) return res.sendStatus(401);
-        const userInDb = getUserById(userId);
-        if (!userInDb) return res.sendStatus(401);
-        console.log(`[postsController]: created new comment for post id:${postId} from user id:${userId}`);
-        if (!ObjectId.isValid(postId) || !(await getPostById(postId))) {
-            console.log(`[postsController]: wrong post id:${postId}`);
-            return res.sendStatus(404);
-        }
-        const {content} = req.body;
-        const createdComment = await createUserComment(content, userId, postId);
-        return createdComment ? res.status(201).send(createdComment) : res.sendStatus(500);
-    });
+    postsController.createCommentForPost.bind(postsController)
+);
 
 postsRouter.get('/:postId/comments',
-    async (req: RequestWithIdAndBody<CommentInputModelDto>, res: Response) => {
-        const postId = req.params.postId;
-        if (!ObjectId.isValid(postId) || !(await getPostById(postId))) return res.sendStatus(404);
-        const paginatorOption: PaginatorOptionInterface = parseQueryPaginator(req);
-        const result = await findAllCommentsByPostId(postId, paginatorOption);
-        return result ? res.status(200).json(result) : res.sendStatus(500);
-    });
+    postsController.getCommentsFOrPost.bind(postsController)
+);
