9e4c188528482c253c4ba231ff1a2fa0edfa2077
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-10 12:37:35 +0200

    refactor to liking comments

---
+++ src/middlewares/authCheckBearer.middleware.ts
@@ -0,0 +1,17 @@
+import {Request, Response, NextFunction} from 'express';
+import {jwtService} from "../utils/jwt-service";
+import {UsersService} from "../services/users.service";
+
+const usersService = new UsersService();
+export const authCheckBearerMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization) {
+        return next();
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    const userId = await jwtService.getUserIdByJwtToken(token, "access");
+    if (userId) {
+        req.user = await usersService.getUserById(userId);
+        return next();
+    }
+    return next();
+};
