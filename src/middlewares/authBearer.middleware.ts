e0c649d82942e0d6d232acc7e160f5dac73c9f1b
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-10 12:37:30 +0200

    refactor to liking comments

--- src/middlewares/authBearer.middleware.ts
+++ src/middlewares/authBearer.middleware.ts
@@ -2,16 +2,25 @@ import {Request, Response, NextFunction} from 'express';
 import {jwtService} from "../utils/jwt-service";
 import {UsersService} from "../services/users.service";
 
-const usersService = new UsersService()
-export const authBearerMiddleware =async (req: Request, res: Response, next: NextFunction) => {
+const usersService = new UsersService();
+export const authBearerMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(`[authBearerMiddleware] started...`);
     if (!req.headers.authorization) {
+        console.log(`[authBearerMiddleware] access denied - no headers.authorization`);
         return res.sendStatus(401);
     }
-    const token= req.headers.authorization.split(' ')[1]
-    const userId=await jwtService.getUserIdByJwtToken(token, "access");
-    if (userId) {
-        req.user= await usersService.getUserById(userId)
-        return next()
+    const token = req.headers.authorization.split(' ')[1];
+    const userId = await jwtService.getUserIdByJwtToken(token, "access");
+    if (!userId) {
+        console.log(`[authBearerMiddleware] access denied - no valid access token`);
+        return res.sendStatus(401);
+    };
+    const user = await usersService.getUserById(userId);
+    if(!user) {
+        console.log(`[authBearerMiddleware] access denied - do not find user in DB`);
+        return res.sendStatus(401);
     }
-    return res.sendStatus(401);
+    req.user = user
+    return next();
+
 };
