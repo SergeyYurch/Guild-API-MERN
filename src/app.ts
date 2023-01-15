15c2b911bdf6fbf9246e7a01ae4e00bbf0aa4fb7
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-15 15:39:23 +0200

    refactor to class % dependency injection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import express, {Express} from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import {blogsRouter} from './routes/blogs.route';
 import {postsRouter} from './routes/posts.route';
@@ -9,18 +9,50 @@ import {authRouter} from './routes/auth.route';
 import {usersRouter} from './routes/users.route';
 import {securityRouter} from './routes/security.route';
 import {commentsRouter} from './routes/comments.route';
-
-export const app = express();
-app.set('trust proxy', true)
-app.use(cors())
-app.use(cookieParser())
-app.use(express.json())
-app.use('/blogs', blogsRouter)
-app.use('/posts', postsRouter)
-app.use('/testing', testingRouter)
-app.use('/auth', authRouter)
-app.use('/users', usersRouter)
-app.use('/comments', commentsRouter)
-app.use('/security', securityRouter)
+import {Server} from 'http';
+import {runDB} from './adapters/dbAdapters';
+import {injectable} from 'inversify';
 
 
+const port = process.env.PORT || 5001;
+
+@injectable()
+export class App {
+    app: Express;
+    port: number;
+    server!: Server;
+
+    constructor() {
+        this.app = express();
+        this.port = +port;
+    }
+
+    useMiddleware(): void {
+        this.app.use(express.json());
+        this.app.use(cookieParser());
+        this.app.use(cors());
+    }
+
+    useRouters(): void {
+        this.app.use('/blogs', blogsRouter);
+        this.app.use('/posts', postsRouter);
+        this.app.use('/testing', testingRouter);
+        this.app.use('/auth', authRouter);
+        this.app.use('/users', usersRouter);
+        this.app.use('/comments', commentsRouter);
+        this.app.use('/security', securityRouter);
+    }
+
+    public async init() {
+        this.app.set('trust proxy', true);
+        this.useMiddleware();
+        this.useRouters();
+        await runDB();
+        this.server = this.app.listen(this.port, () => {
+            console.log(`[App] Server on post:${this.port} is start `);
+        });
+    }
+    public close(): void{
+        this.server.close()
+    }
+}
