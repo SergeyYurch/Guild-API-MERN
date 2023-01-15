52c61367dbc31e835a687a016f570f340556b780
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-15 15:42:56 +0200

    refactor to class % dependency injection

--- src/routes/testing.route.ts
+++ src/routes/testing.route.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import {testingController} from '../composition-root/compositiomRoot';
+import {TestingController} from '../controllers/testing.controller';
 
+// const testingController = appContainer.get(TestingController)
 export const testingRouter = Router();
 
 testingRouter.delete('/all-data',
