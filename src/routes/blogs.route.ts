775a8722b85f6b0028d0b506774d260c341d5a94
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-15 15:40:27 +0200

    refactor to class % dependency injection

--- src/routes/blogs.route.ts
+++ src/routes/blogs.route.ts
@@ -1,8 +1,24 @@
 import {Router} from "express";
+import 'reflect-metadata';
+
 import {validatorMiddleware} from "../middlewares/validator.middleware";
 import {authBasicMiddleware} from "../middlewares/authBasic.middleware";
+// import {blogsController} from '../composition-root/compositiomRoot';
+import {BlogsController} from '../controllers/blogs.controller';
+import {TYPES} from '../types/types';
+import {BlogsService} from '../services/blogs.service';
+import {BlogsRepository} from '../repositories/blogs.repository';
+import {QueryRepository} from '../repositories/query.repository';
+import {PostsService} from '../services/posts.service';
+import {UsersRepository} from '../repositories/users.repository';
+import {PostsRepository} from '../repositories/posts.repository';
 import {blogsController} from '../composition-root/compositiomRoot';
 
+// const blogsController = new BlogsController(
+//     new BlogsService(new BlogsRepository(), new QueryRepository(new UsersRepository())),
+//     new QueryRepository(new UsersRepository()),
+//     new PostsService(new PostsRepository(), new QueryRepository(new UsersRepository())))
+// const blogsController = appContainer.get<BlogsController>(TYPES.BlogsController)
 export const blogsRouter = Router();
 
 const {
