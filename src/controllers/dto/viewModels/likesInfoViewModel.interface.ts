62e5abacf2867d8acfc4573f624c1d73a715780b
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-19 21:01:10 +0200

    added like status for posts

--- src/controllers/dto/viewModels/likesInfoViewModel.interface.ts
+++ src/controllers/dto/viewModels/likesInfoViewModel.interface.ts
@@ -3,5 +3,5 @@ import {LikeStatusType} from '../../../repositories/interfaces/likeStatus.type';
 export interface LikesInfoViewModelInterface{
     likesCount: number;
     dislikesCount: number;
-    myStatus: LikeStatusType
+    myStatus: LikeStatusType;
 }
