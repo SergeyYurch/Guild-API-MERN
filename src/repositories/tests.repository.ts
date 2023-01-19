52ea8911dbe118829e3019e0bcf4a696fe974443
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-01-19 21:01:49 +0200

    added like status for posts

--- src/repositories/tests.repository.ts
+++ src/repositories/tests.repository.ts
@@ -2,25 +2,28 @@ import {
     AccessAttemptModel, BlogModel,
     CommentModel,
     SessionModel, UserModel,
-    PostModel,
+    PostModel, LikeModel,
 } from "../adapters/dbAdapters";
 import {injectable} from 'inversify';
 
 @injectable()
 export class TestsRepository {
-    async dataBaseClear(): Promise<boolean>{
-        console.log(`[repository]:start dataBaseClear`);
-        const resultBlogs = await BlogModel.deleteMany({});
-        const resultPosts = await PostModel.deleteMany({});
-        const resultUsers = await UserModel.deleteMany({});
-        const resultComments = await CommentModel.deleteMany({});
-        const resultDeviceAuthSession = await SessionModel.deleteMany({});
-        const resultAccessAttempt = await AccessAttemptModel.deleteMany({});
-        return resultBlogs.acknowledged
-            && resultPosts.acknowledged
-            && resultUsers.acknowledged
-            && resultComments.acknowledged
-            && resultDeviceAuthSession.acknowledged
-            && resultAccessAttempt.acknowledged;
+    async dataBaseClear(): Promise<boolean> {
+        await console.log(`[repository]:start dataBaseClear`);
+        const blogsDeleteResult = await BlogModel.deleteMany({});
+
+        const postsDeleteResult = await PostModel.deleteMany({});
+        const usersDeleteResult = await UserModel.deleteMany({});
+        const commentsDeleteResult = await CommentModel.deleteMany({});
+        const sessionsDeleteResult = await SessionModel.deleteMany({});
+        const accessAttemptsDeleteResult = await AccessAttemptModel.deleteMany({});
+        const likesDeleteResult = await LikeModel.deleteMany({});
+        return blogsDeleteResult.acknowledged
+            && postsDeleteResult.acknowledged
+            && usersDeleteResult.acknowledged
+            && commentsDeleteResult.acknowledged
+            && sessionsDeleteResult.acknowledged
+            && accessAttemptsDeleteResult.acknowledged
+            && likesDeleteResult.acknowledged;
     }
 }
